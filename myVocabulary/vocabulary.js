5b196abe1ca666fdf0aff8588b78071c2eaab718
Ariella1315 <ariel50161@gmail.com>
2025-07-21 17:26:46 +0800

    Create vocabulary.js

---
+++ myVocabulary/vocabulary.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let html = '<div class="box">';
+html += 'WORDS';
+html += '</div>';
+html += '<button class="key">NEXT</button>';
+document.getElementById("context").innerHTML=html;
